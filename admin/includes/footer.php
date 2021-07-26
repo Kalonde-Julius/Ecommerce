91a7a981baab5a3f309479bc9158557f10b0e684
Kalonde Julius <58883291+Kalonde-Julius@users.noreply.github.com>
2021-07-26 13:20:34 +0300

    initial commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2021 <a href="https://Kalonde-Julius.github.io/Kalonde22"> Kalonde Julius Seguya</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2021 <a href="https://Kalonde-Julius.github.com/Kalonde22">Kalonde Julius Seguya</a></strong>
+    </div>
+</footer>
